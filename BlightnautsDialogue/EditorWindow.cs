b97ae62da12109667227d50c01f16286a2b9c98f
CationF <80769076+CationF@users.noreply.github.com>
2021-05-12 13:28:33 +0100

    Initial commit

---
+++ BlightnautsDialogue/EditorWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlightnautsDialogue
+{
+    public partial class EditorWindow : Form
+    {
+        public EditorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void topBarNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topBarOpen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topBarSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topBarSaveAs_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topBarExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void topBarNewTrigger_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void topBarLoadTrigger_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- BlightnautsDialogue/Program.cs
+++ BlightnautsDialogue/Program.cs
@@ -13,17 +13,16 @@ namespace BlightnautsDialogue
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormTeam());
-            // Disabled for now
-            ProjectManager.LoadProject();
-            FolderBrowserDialog dialog = new FolderBrowserDialog();
-            DialogResult result = dialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                Exporter.DialogueExporter.Export(dialog.SelectedPath + "\\AnimationTemplates");
-                Exporter.BehaviourExporter.Export(dialog.SelectedPath + "\\Behaviours");
-                Exporter.MapExporter.Export(dialog.SelectedPath + "\\Maps\\externalToolTest\\Gameplay.xml");
-            }
+            Application.Run(new EditorWindow());
+            //ProjectManager.LoadProject();
+            //FolderBrowserDialog dialog = new FolderBrowserDialog();
+            //DialogResult result = dialog.ShowDialog();
+            //if (result == DialogResult.OK)
+            //{
+            //    Exporter.DialogueExporter.Export(dialog.SelectedPath + "\\AnimationTemplates");
+            //    Exporter.BehaviourExporter.Export(dialog.SelectedPath + "\\Behaviours");
+            //    Exporter.MapExporter.Export(dialog.SelectedPath + "\\Maps\\externalToolTest\\Gameplay.xml");
+            //}
         }
     }
 }
